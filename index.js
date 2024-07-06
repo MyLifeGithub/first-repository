095671dd72969c833b903a92e36463cc06c4053b
Diyor <sitoraulmasova@gmail.com>
2024-07-06 15:51:57 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let random = Math.random()
+let string = random.toString()
+let result = string.replace('.','')
+console.log(result);
+
+parseFloat()
+parseInt()
+Math.random()
+Math.round()
+Math.ceil()
+Math.floor()
